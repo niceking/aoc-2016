44bcde7c6c5d094198f6d676193d1043c018d7c2
Rose Lu <roseyylu@gmail.com>
2016-12-23 22:39:32 +0800

    d4 p2

--- 4/rooms.rb
+++ 4/rooms.rb
@@ -2,16 +2,17 @@ input = File.read("input.txt").split("\n")
 
 input.map! do |l|
   sp = l.split('-')
-  [sp.take(sp.size - 1).join.chars.sort.join, sp.last]
+  [sp.take(sp.size - 1), sp.last]
 end
 
 total = 0
+legit_rooms = []
 
 input.each do |i|
   alpha_count = {}
 
   ('a'..'z').each do |c|
-    alpha_count[c] = i[0].count(c)
+    alpha_count[c] = i[0].join.count(c)
   end
 
   alpha_count = alpha_count.sort do |l, r|
@@ -28,8 +29,30 @@ input.each do |i|
 
   secID, checksum = i[1].split('[')
   checksum.slice!(']')
+  secID = secID.to_i
 
-  total += secID.to_i if name == checksum
+  if name == checksum
+    total += secID
+    legit_rooms.push([i[0].join(" "), secID])
+  end
 end
 
 puts total
+
+##########################
+
+puts "\npart 2\n"
+
+alphabet = ('a'..'z').to_a
+
+legit_rooms.map! do |rm|
+  offset = rm[1] % 26 - 26
+  phrase = rm[0].chars
+  phrase.map! { |c| alphabet[alphabet.find_index(c) + offset] if alphabet.find_index(c) }
+
+  [phrase.join, rm[1]]
+end
+
+legit_rooms.each do |r|
+  puts r[1] if r[0].include?('northpoleobjects')
+end
